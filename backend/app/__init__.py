b35691bb3f4e2671ac3fb9db84a3701bd0a086ca
lhanif <lhanif467@gmail.com>
2025-12-17 02:41:51 +0700

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
-    cors.init_app(app, supports_credentials=True,origins="http://localhost:5173")
+    cors.init_app(app, supports_credentials=True)
     
     register_routes(app)
 
